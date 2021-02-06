d73ab3b48d75cacc6440cc9acfdc6c96513c18c5
Freddie Jonas <fj6787@gmail.com>
2021-02-06 15:52:54 +0000

    Bot now reads data from config.json

--- PumpBot.py
+++ PumpBot.py
@@ -1,37 +1,37 @@
 from binance.client import Client
 import math
-import time
-from datetime import datetime
-import decimal
+import json
 
 def float_to_string(number, precision=10):
     return '{0:.{prec}f}'.format(
         number, prec=precision,
     ).rstrip('0').rstrip('.') or '0'
 
-# api keys
-apiKey = ''
-apiSecret = ''
-# max amount for buy limit to pay
-buyLimit = 1.15
+# read json file
+f = open('config.json',)
+data = json.load(f)
+apiKey = data['apiKey']
+apiSecret = data['apiSecret']
+profitMargin = data['profitMargin']
+percentOfWallet = data['percentOfWallet']
+buyLimit = data['buyLimit']
 client = Client(apiKey, apiSecret)
-profitMargin = float(input("Profit(%): ")) / 100
-percentOfWallet = float(input("Percent of wallet(%): ")) / 100
+
 # find amount of bitcoin to use
 BTCBalance = float(client.get_asset_balance(asset='BTC')['free'])
 BTCtoSell = BTCBalance * percentOfWallet
 # wait until coin input
 tradingPair = input("Coin: ").upper() + 'BTC'
 
+# get trading pair price
 price = float(client.get_avg_price(symbol=tradingPair)['price'])
+# calculate amount of coin to buy
 amountOfCoin = BTCtoSell / price;
 
 # rounding the coin to the specified lot size
 info = client.get_symbol_info(tradingPair)
 minQty = float(info['filters'][2]['minQty'])
 amountOfCoin = float_to_string(amountOfCoin, int(- math.log10(minQty)))
-
-
 
 # find average price in last 30 mins
 agg_trades = client.aggregate_trade_iter(symbol=tradingPair, start_str='30 minutes ago UTC')
@@ -45,7 +45,7 @@ minPrice = minQty = float(info['filters'][0]['minPrice'])
 averagePrice = float_to_string(averagePrice, int(- math.log10(minPrice)))
 averagePrice = averagePrice * buyLimit
 
-# # buy order
+# buy order
 order = client.order_limit_buy(
     symbol=tradingPair, 
     quantity=amountOfCoin,
