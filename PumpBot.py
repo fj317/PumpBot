bc4415dc479bd9856a64e1a7814b217b58de6486
ezanchi <eroszansan@gmail.com>
2021-02-14 22:21:56 +0000

    Revert "Patch 1" (#2)
    * Revert "Update PumpBot.py"
    
    This reverts commit 7f0cf209a7894ae3ccf9d72a7cc204208421969a.
    
    * Revert "Update config.json"
    
    This reverts commit 23ad26bd5c4d87ef43d299a24527f6debe3e6543.
    
    * Revert "Update PumpBot.py"
    
    This reverts commit 1b20c544b44eeab30eb64c2f6e0503922acfdd19.
    
    * Revert "Open Binance trading pair if setting is set to TRUE in config file"
    
    This reverts commit a02826e092d68443ae4d68a4b332d6c2f600b85f.
    
    * Revert "Add setting for opening binance webpage after trade"
    
    This reverts commit 05789d96f022336def73d8c4b96792b0a21dd273.
    
    * Revert "Bot opens binance page of trading pair once sell order has been made"
    
    This reverts commit 9aee5f97c34fdfbbbddc5d00150a92b1636ccc99.

--- PumpBot.py
+++ PumpBot.py
@@ -3,7 +3,6 @@ from binance.enums import *
 from binance.exceptions import *
 import math
 import json
-import webbrowser
 
 def float_to_string(number, precision=10):
     return '{0:.{prec}f}'.format(
@@ -20,9 +19,7 @@ percentOfWallet = float(data['percentOfWallet']) / 100
 buyLimit = data['buyLimit']
 stopLoss = data['stopLoss']
 coinPair = data['coinPair']
-getAveragePrice = data['getPastTrades']
-minutesAveragePrice = data['minutesAveragePrice']
-openBinanceWeb = data['openBinanceWeb']
+getPastTrades = data['getPastTrades']
 client = Client(apiKey, apiSecret)
 
 # find amount of bitcoin to use
@@ -64,9 +61,9 @@ except Exception as d:
 amountOfCoin = BTCtoSell / price
 
 # ensure buy limit is setup correctly
-if(getAveragePrice == "TRUE"):
+if(getPastTrades == "TRUE"):
     # find average price in last 2 mins
-    agg_trades = client.aggregate_trade_iter(symbol=tradingPair, start_str=minutesAveragePrice + " minutes ago UTC")
+    agg_trades = client.aggregate_trade_iter(symbol=tradingPair, start_str='2 minutes ago UTC')
     agg_trade_list = list(agg_trades)
     total = 0
     for trade in agg_trade_list:
@@ -138,6 +135,3 @@ except Exception as d:
     quit()
 
 print('Sell order has been made!')
-# open binance page to trading pair
-if (openBinanceWeb == "TRUE"):
-    webbrowser.open('https://www.binance.com/en/trade/' + tradingPair)
