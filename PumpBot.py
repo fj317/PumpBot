9cdfe66e58c8e685f3d589ec1b930c935153f02d
ezanchi <eroszansan@gmail.com>
2021-02-16 19:28:07 +0000

    Fixed warning text and Standards

--- PumpBot.py
+++ PumpBot.py
@@ -21,10 +21,10 @@ logfile = open("log.txt", "w+")
 
 # read json file
 try:
-    f = open('key.json', )
+    f = open('keys.json', )
 except FileNotFoundError:
     log("Keys.json not found")
-    sys.exit("Remember to rename your key.json.example to key.json.")
+    sys.exit("Remember to rename your keys.json.example to keys.json.")
     
 data = json.load(f)
 apiKey = data['apiKey']
